2ab2c29d53cffe1d8650f89d1834db5003535900
Arthur Henrique <arthurcostalima88@gmail.com>
2024-08-27 02:06:56 -0300

    last

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+function mult (num1: number, num2: number): number {
+    return num1 * num2;
+}
+
+function dizOI (nome: string) {
+    console.log(`${nome} disse oi.`)
+}
+
+console.log(mult(7, 2))
+
+console.log(dizOI('Arthur'))
